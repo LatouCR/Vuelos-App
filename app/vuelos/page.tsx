70df9af62d6642c66c96d040e5ef5e65cb331714
LatouCR <dlatouches811@ulacit.ed.cr>
2023-12-09 10:40:47 -0600

    Add existing project files to Git

---
+++ app/vuelos/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+      <main className="bg-background">
+        <div className='flex items-center justify-center h-screen'>
+          <h1 className='text-6xl text-slate-800'>Vuelos</h1>
+        </div>
+      </main>
+    )
+  }
+  
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,17 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'text': '#000000',
+        'txt-2': '#0c1a3c',
+        'background': '#fcfcfc',
+        'background-2': '#0032A0',
+        'primary': '#0245d4',
+        'secondary': '#0c1a3c',
+        'secondary-light': '#374975',
+        'accent': '#f59700',
+        'accent-light': '#f2ae40',
+       },
     },
   },
   plugins: [],
